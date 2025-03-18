de54efc1e3ee054d6345e02e5e95efa4d7b1ff19
Qian Cheng <91555901+qcheng-dev@users.noreply.github.com>
2025-03-18 08:34:30 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/jtuaa-sea-web',
+  base: '/jtuaa-sea-web/',
   server: {
     port: 3000,
     host: true
